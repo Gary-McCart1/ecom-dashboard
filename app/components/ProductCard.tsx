93c6a525782c15aa1b6393f165b9445883bb4f51
Gary McCart <gwmccart3@gmail.com>
2025-05-19 15:22:09 -0400

    fix order page

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,24 +1,45 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import { OrderItem } from "../types/orderItems";
 import Image from "next/image";
 import Link from "next/link";
 import Loading from "../loading";
+import { Product } from "../types/product";
 
 interface Props {
   item: OrderItem;
 }
 
 const ProductCard = ({ item }: Props) => {
-  console.log(item)
-  if (!item) return <Loading />
-  const imageUrl = item.product?.images?.[0]?.url ?? "";
+  const [product, setProduct] = useState<Product | null>(null);
+
+  useEffect(() => {
+    if (!item?.product) return;
+
+    const fetchProduct = async () => {
+      try {
+        const response = await fetch(
+          `https://foamhead-a8f24bda0c5b.herokuapp.com/api/products/${item.product}`
+        );
+        if (!response.ok) throw new Error("Failed to fetch product");
+        const data = await response.json();
+        setProduct(data);
+      } catch (error) {
+        console.error("Error fetching product:", error);
+      }
+    };
+
+    fetchProduct();
+  }, [item.product]);
+
+  if (!item || !product) return <Loading />;
 
   return (
     <div className="my-10">
       <div className="card card-side bg-white shadow-sm">
         <figure>
           <Image
-            src={imageUrl}
+            src={product.images[0].url}
             width={100}
             height={100}
             alt="Movie"
@@ -26,16 +47,18 @@ const ProductCard = ({ item }: Props) => {
         </figure>
         <div className="card-body">
           <h2 className="card-title">
-            {item.product.name} - {item.product.category}
+            {product.name} - {product.category}
           </h2>
-          <p>Rating: {item.product.rating}/5.0</p>
+          <p>Rating: {product.rating}/5.0</p>
           <p>Quantity: {item.quantity}</p>
-          <p>Price: ${item.product.price}</p>
-          <p dangerouslySetInnerHTML={{
-                __html: item.product.description.substring(0, 150) + "...",
-              }}></p>
+          <p>Price: ${product.price}</p>
+          <p
+            dangerouslySetInnerHTML={{
+              __html: product.description.substring(0, 150) + "...",
+            }}
+          ></p>
           <div className="card-actions justify-end">
-            <Link href={`/products/${item.product.id}`}>
+            <Link href={`/products/${product.id}`}>
               <button className="btn btn-black">View Product</button>
             </Link>
           </div>
